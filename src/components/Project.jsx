c4df0f2164964975ac2e8c0a1c990967ddb38e34
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-07-12 20:28:51 +0100

    view live

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -29,7 +29,7 @@ function Project() {
                 <h1 className="text-3xl font-semibold text-center text-gray-200 uppercase font-poppins">Projects</h1>
                 <p className="max-w-xl mb-10 text-lg text-center text-gray-400 font-lato">You might want to develop a website, application or discuss anything related. Feel free to leave a message below</p>
 
-                <div className="grid gap-8 px-8 grid md:grid-cols-2 xl:grid-cols-3">
+                <div className="grid gap-8 px-8 md:grid-cols-2 xl:grid-cols-3">
 
                     {projects.slice(0, projectsToShow).map((project) => (
                         <div key={project.id} className="p-5 text-gray-300 bg-gray-800 rounded-lg h-full md:w-full border-[0.5px] border-gray-600 hover:-translate-y-2 transition-all duration-300">
@@ -45,7 +45,7 @@ function Project() {
                             <p className="text-gray-300">{project.description}</p>
 
                             <div className="flex gap-3 mt-3">
-                                <LiveButton link={project.liveLink}>View Website <MdArrowOutward className="text-2xl" /></LiveButton>
+                                <LiveButton link={project.liveLink}>View Live <MdArrowOutward className="text-2xl" /></LiveButton>
                                 <GithubButton link={project.githubLink}>Github <FaGithub /></GithubButton>
                             </div>
                         </div>
